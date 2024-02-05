81321ae86f9e3ef2078e8af39e6bbc8f5a116c6a
MaximUser0 <makcim.o.d@gmail.com>
2024-02-05 16:09:01 +0400

    djugpodfjgidfj

--- game.js
+++ game.js
@@ -1,7 +1,4 @@
-// window.addEventListener('beforeunload', function (e) {
-//     e.preventDefault();
-//     alert("sfjhoisgyhdyhfs");
-// })
+
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
@@ -21,8 +18,8 @@ pipeBottom.src = "img/pipeBottom.png";
 var fly = new Audio();
 var score_audio = new Audio();
 
-fly.src = "audio/fly.mp3";
-score_audio.src = "audio/score.mp3";
+// fly.src = "audio/fly.mp3";
+// score_audio.src = "audio/score.mp3";
 
 var gap = 90;
 
@@ -30,16 +27,16 @@ var gap = 90;
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
-    yPos -= 25;
-    fly.play();
+ yPos -= 25;
+ fly.play();
 }
 
 // Создание блоков
 var pipe = [];
 
 pipe[0] = {
-    x: cvs.width,
-    y: 0
+ x : cvs.width,
+ y : 0
 }
 
 var score = 0;
@@ -49,46 +46,46 @@ var yPos = 150;
 var grav = 1.5;
 
 function draw() {
-    ctx.drawImage(bg, 0, 0);
+ ctx.drawImage(bg, 0, 0);
 
+ for(var i = 0; i < pipe.length; i++) {
+ ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+ ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
-    for (var i = 0; i < pipe.length; i++) {
-        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+ pipe[i].x--;
 
-        pipe[i].x--;
+ if(pipe[i].x == 125) {
+ pipe.push({
+ x : cvs.width,
+ y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+ });
+ }
 
-        if (pipe[i].x == 125) {
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-            });
-        }
+ // Отслеживание прикосновений
+ if(xPos + bird.width >= pipe[i].x
+ && xPos <= pipe[i].x + pipeUp.width
+ && (yPos <= pipe[i].y + pipeUp.height
+ || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+ location.reload(); // Перезагрузка страницы
+ }
 
-        // Отслеживание прикосновений
-        if (xPos + bird.width >= pipe[i].x
-            && xPos <= pipe[i].x + pipeUp.width
-            && (yPos <= pipe[i].y + pipeUp.height
-                || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
-            location.reload(); // Перезагрузка страницы
-        }
+ if(pipe[i].x == 5) {
+ score++;
+ score_audio.play();
+ }
+ }
 
-        if (pipe[i].x == 5) {
-            score++;
-            score_audio.play();
-        }
-    }
+ ctx.drawImage(fg, 0, cvs.height - fg.height);
+ ctx.drawImage(bird, xPos, yPos);
 
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, xPos, yPos);
+ yPos += grav;
 
-    yPos += grav;
+ ctx.fillStyle = "#000";
+ ctx.font = "24px Verdana";
+ ctx.fillText("Счет: " + score, 10, cvs.height - 20);
 
-    ctx.fillStyle = "#000";
-    ctx.font = "24px Verdana";
-    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
-
-    requestAnimationFrame(draw);
+ requestAnimationFrame(draw);
 }
 
 pipeBottom.onload = draw;
+
